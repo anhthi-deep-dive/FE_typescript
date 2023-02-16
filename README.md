6754a6463fcf57580bc5e69a271700fd43b90fe4
Thi Nguyen <anhthi.ieig@gmail.com>
2023-02-16 07:41:17 +0700

    Narrowing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # typescript
+
+https://www.typescriptlang.org/
---
+++ Refer.ts
@@ -0,0 +1,4 @@
+let personIndex: number | string = Math.random() < 0.5 ? "10" : 10;
+
+personIndex = 20;
+console.log(personIndex); // let personIndex: number
